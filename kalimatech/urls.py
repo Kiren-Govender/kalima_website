393bd2b1ff4282eeccf98b9b62a75b6c4d2f81cd
Kiren-Govender <109963236+Kiren-Govender@users.noreply.github.com>
2023-01-07 18:31:12 +0200

    Added maintenance app

--- kalimatech/urls.py
+++ kalimatech/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("mainsite.urls"))
+    path("", include("mainsite.urls")),
+    path("maintenance/", include("maintenance.urls"))
 ]
