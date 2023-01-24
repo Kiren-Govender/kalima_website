395ccdcc4304c60b28bca14e3a1f4cec2cf946a0
Kiren-Govender <109963236+Kiren-Govender@users.noreply.github.com>
2023-01-24 23:03:02 +0200

    Changed static folder

--- kalimatech/settings.py
+++ kalimatech/settings.py
@@ -123,9 +123,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 #Location of static files
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets'), ]
 
-STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT  = os.path.join(BASE_DIR, 'static')
 
 
 # Default primary key field type
