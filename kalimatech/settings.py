1f82cf9581e5fa2152df4f5784aca6a4969952e9
Kiren-Govender <109963236+Kiren-Govender@users.noreply.github.com>
2023-01-24 22:26:59 +0200

    Static file changes to django.

--- kalimatech/settings.py
+++ kalimatech/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -118,7 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+
+#Location of static files
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+
+STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
